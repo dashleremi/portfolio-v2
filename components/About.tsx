5d061fe816b0580272fdefa3fbc86a2b406694c7
dashleremi <dashleremi@gmail.com>
2024-10-24 15:02:28 -1000

    first commit

---
+++ components/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id='about' className='mt-20 flex flex-col items-center text-center'>
+        <h1>
+            Welcome to my <span>Portfolio</span>
+        </h1>
+    </div>
+  )
+}
+
+export default About
