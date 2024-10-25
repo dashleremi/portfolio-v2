4514e47c879f7dec9b4fd9a247253197b7d3e5ca
dashleremi <dashleremi@gmail.com>
2024-10-24 15:49:16 -1000

    homepage elements, mouse effect, glass morphism component

--- components/About.tsx
+++ components/About.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div id='about' className='mt-20 flex flex-col items-center text-center'>
-        <h1>
-            Welcome to my <span>Portfolio</span>
-        </h1>
+    <div id='about' className='flex flex-col items-center justify-center mt-32'>
+        <h1 className='text-white text-[40px] font-extralight'>About</h1>
+        <div className='mt-20'>
+            <h2 className='text-3xl'>Welcome to my Portfolio !</h2>
+        </div>
+
     </div>
   )
 }
--- components/Home.tsx
+++ components/Home.tsx
@@ -5,20 +5,20 @@ const Home = () => {
     <div className="">
       <div className="absolute top-5 right-5 text-[25px]">contact</div>
 
-      <h1 className="absolute left-16 w-full text-[#000000] text-[250px] tracking-wide font-bold font font-secondary opacity-5">
+      <h1 className="absolute left-16 text-[#000000] text-[250px] tracking-wide font-bold font-secondary opacity-5">
         RAMIAEMI
       </h1>
       <div className="bottom-40">
-        <h1 className="absolute left-32 top-12 w-full text-[#000000] text-[250px] tracking-wide font-bold font font-secondary">
+        <h1 className="absolute left-32 top-12 text-[#000000] text-[250px] tracking-wide font-bold font-secondary">
           RAMIAEMI
         </h1>
       </div>
 
-      <h1 className="absolute bottom-0 right-10 text-[#000000] text-[250px] tracking-wide font-bold font font-secondary opacity-5">
+      <h1 className="absolute bottom-0 right-10 text-[#000000] text-[250px] tracking-wide font-bold font-secondary opacity-5">
         DASHLER
       </h1>
       <div className="-right-10">
-        <h1 className="absolute bottom-10 right-32 text-[#000000] text-[250px] tracking-wide font-bold font font-secondary">
+        <h1 className="absolute bottom-10 right-32 text-[#000000] text-[250px] tracking-wide font-bold font-secondary">
           DASHLER
         </h1>
       </div>
@@ -27,7 +27,7 @@ const Home = () => {
         <div className="flex items-center justify-center gap-72">
           <p className="text-3xl line-effect">about</p>
           <p className="text-3xl line-effect">skills</p>
-          <p className="text-8xl text-[#ffffff] font-bold w-[350px] h-[300px] rounded-[100px] bg-[#a8a8a839] items-center text-center justify-center flex">
+          <p className="text-8xl text-[#ffffff] font-bold w-[350px] h-[300px] rounded-[100px] bg-[#c8c8c839] items-center text-center justify-center flex">
             Aloha
           </p>
           <p className="text-3xl line-effect">experience</p>
