a666fc99b3d80e7fc75313376cc6e1671cd4caae
dashleremi <dashleremi@gmail.com>
2024-11-05 22:34:56 -1000

    project layout, resume update, case studies

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -10,21 +10,23 @@ const Projects = () => {
             <h1 className='text-white text-[40px] text-center font-extralight'>Projects</h1>
 
             <div className='w-[60%] mx-auto'>
-                <div className='grid grid-cols-1 items-start mx-auto'>
+                <div className='grid grid-cols-1 items-start mx-auto rounded-md'>
                     <p className='mt-20 text-2xl uppercase font-bold'>Case Studies</p>
                 </div>
 
                 <div className='grid grid-cols-2 items-center justify-center mt-20'>
-                    <div>
+                    <div className='glassmorphism-container'>
                         <h1>Mint Case Study</h1>
                     </div>
-                    <div>
+                    <div className='glassmorphism-container'>
                         <h1>Netflix Redesign, Case Study</h1>
                     </div>
                 </div>
 
                 <div className='grid grid-cols-1 items-center jutify-center mt-20'>
-                    <h1>Coding with kids</h1>
+                    <div className='glassmorphism-container'>
+                        <h1>Coding with kids</h1>
+                    </div>
                 </div>
             </div>
             </section>
