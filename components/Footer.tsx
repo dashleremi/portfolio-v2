7f9d5ebeb8855fa141b2d194e8a9f8e52f4c8e9a
dashleremi <dashleremi@gmail.com>
2024-11-06 12:01:56 -1000

    footer update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,16 +8,16 @@ const Footer = () => {
         <div className='max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8'>
             <nav className='flex flex-wrap justify-center -mx-5 -my-2'>
                 <div className='px-10 py-2'>
-                    <Link href="/home">Home</Link>
+                    <Link href="/homepage">Home</Link>
                 </div>
                 <div className='px-10 py-2'>
-                    <Link href="/about">About</Link>
+                    <Link href="/aboutme">About</Link>
                 </div>
                 <div className='px-10 py-2'>
                     <Link href="/projects">Projects</Link>
                 </div>
                 <div className='px-10 py-2'>
-                    <Link href="/resume">Resume</Link>
+                    <Link href="/myresume">Resume</Link>
                 </div>
             </nav>
 
