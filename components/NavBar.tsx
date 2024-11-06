eb404e9c6898cf472ca9b25db8c9df09d5b0bca5
dashleremi <dashleremi@gmail.com>
2024-11-06 11:58:39 -1000

    new pagination layout

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     <div className='mt-10 fixed top-0 left-0 z-10 w-full flex items-center justify-between'>
         <div className='px-10 py-3 container mx-auto flex items-center justify-between rounded-full border border-t border-[#66666689] w-[50%] backdrop-blur'>
             <div>
-            <Link id='' href="/home" className='hover:font-bold'>Home</Link>
+            <Link id='' href="/homepage" className='hover:font-bold'>Home</Link>
             </div>
             <div className='flex items-center gap-10'>
             <Link href="/aboutme" className='hover:font-bold'>About</Link>
