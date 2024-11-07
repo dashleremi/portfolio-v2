73857b51280d8a79df5495e9101f1d293e672dca
dashleremi <dashleremi@gmail.com>
2024-11-06 17:46:15 -1000

    skills update, resume update, new projects

--- pages/myresume.tsx
+++ pages/myresume.tsx
@@ -12,8 +12,35 @@ const Resume = () => {
         
         <section className='mt-40'>
           <h1 className='text-white text-[40px] text-center font-extralight'>Resume</h1>
+          
+          <div className='w-[60%] grid grid-cols-1 items-center mx-auto'>
+            <p className='mt-20 text2xl uppercase font-bold text-2xl'>Skills</p>
+
+            <div className='grid grid-cols-2 items-start justify-center gap-10'>
+              <div className='p-10 mt-10 glassmorphism-container'>
+                <h2 className='text-2xl font-bold'>Technical</h2>
+                <div className='mt-5'>
+                  <p>HTML, CSS/SCSS/SASS, JavaScript/TypeScript, Java, Python</p>
+                  <p>React, Next.js, Vue.js, Node.js, TailwindCSS</p>
+                  <p>Swift, Flutter, C/C++, Figma, Sketch, Storybook</p>
+                  <p>Git, Slack, Jira, MongoDB, PyTorch, Flask, PostgreSQL, Vercel</p>
+                  <p>Adobe XD, Photoshop, Premiere Pro, InDesign, Illustrator</p>
+                </div>
+                <p></p>
+              </div>
+              <div className='p-10 mt-10 glassmorphism-container'>
+                <h2 className='text-2xl font-bold'>Soft</h2>
+                <div className='mt-5'>
+                  <p>Detail-Oriented, Leadership, Communication, Problem-Solving</p>
+                  <p>Critical Thinking, Work Ethic, Collaboration, Adaptability</p>
+                  <p>Creativity, Time Management, Self-Starter, Quick Learner</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
           <div className='w-[60%] grid grid-cols-1 items-start mx-auto'>
-            <p className='mt-20 text-2xl uppercase font-bold'>Education</p>
+            <p className='mt-40 text-2xl uppercase font-bold'>Education</p>
             <div className='mt-10 flex items-center justify-between glassmorphism-container p-10'>
               <div>
                 <h2 className='font-bold text-2xl mb-5'>Florida International University</h2>
@@ -63,8 +90,9 @@ const Resume = () => {
             <Link href="/project1">
               <div className='mt-10 flex items-center justify-between glassmorphism-container p-10 hover:bg-[#d2d2d23f] transition-all duration-300'>
                 <div>
-                  <h2 className='font-bold text-2xl mb-5'>J&apos;s International School</h2>
-                  <p className='text-xl'>Sep 2024 - Oct 2024</p>
+                  <h2 className='font-bold text-2xl mb-2'>J&apos;s International School</h2>
+                  <p>Frontend/UI Developer</p>
+                  <p className='text-xl mt-5'>Sep 2024 - Oct 2024</p>
                   <p className='mt-5'>Designed and developed the frontend/UI for a school management dashboard, incorporating both English and Japanese<br /> language support for seamless user experience across regions.</p>
                 </div>
                 <Image src="/js.png" alt="" className='w-[80px] h-[80px] opacity-50' width={224} height={240}/>
@@ -73,8 +101,9 @@ const Resume = () => {
 
               <div className='mt-10 flex items-center justify-between glassmorphism-container p-10'>
                 <div>
-                  <h2 className='font-bold text-2xl mb-5'>Hawaii Technology Development Corporation</h2>
-                  <p className='text-xl'>Nov 2023 - Aug 2024</p>
+                  <h2 className='font-bold text-2xl mb-2'>Hawaii Technology Development Corporation</h2>
+                  <p>Software Engineer Intern</p>
+                  <p className='text-xl mt-5'>Nov 2023 - Aug 2024</p>
                   <p className='mt-5'>Developed front-end components using HTML, CSS, React, and TypeScript, ensuring cross-browser compatibility and <br /> seamless  integration  with backend systems.</p>
                 </div>
                 <Image src="/htdc.png" alt="" className='w-[90px] h-[90px] opacity-50' width={224} height={240}/>
@@ -84,8 +113,9 @@ const Resume = () => {
             <Link href="/project2">
               <div className='mt-10 flex items-center justify-between glassmorphism-container p-10 hover:bg-[#d2d2d23f] transition-all duration-300'>
                 <div>
-                  <h2 className='font-bold text-2xl mb-5'>CHANEL</h2>
-                  <p className='text-xl'>May 2023 - Oct 2023</p>
+                  <h2 className='font-bold text-2xl mb-2'>CHANEL</h2>
+                  <p>UX/UI Design Intern</p>
+                  <p className='text-xl mt-5'>May 2023 - Oct 2023</p>
                   <p className='mt-5'>Maintained design documentation and style guides across multiple projects, modernized UX/UI designs using Adobe XD  <br /> and Figma, and conducted user research to improve usability.</p>
                 </div>
                 <Image src="/chanel.png" alt="" className='w-[150px] h-[90px] -mr-8 opacity-50' width={224} height={240}/>
@@ -95,8 +125,9 @@ const Resume = () => {
             <Link href="/project4">
               <div className='mt-10 flex items-center justify-between glassmorphism-container p-10 hover:bg-[#d2d2d23f] transition-all duration-300'>
                 <div>
-                  <h2 className='font-bold text-2xl mb-5'>Anthropologie</h2>
-                  <p className='text-xl'>Aug 2022 - May 2023</p>
+                  <h2 className='font-bold text-2xl mb-2'>Anthropologie</h2>
+                  <p>Frontend Development Intern</p>
+                  <p className='text-xl mt-5'>Aug 2022 - May 2023</p>
                   <p className='mt-5'>Redesigned and optimized responsive web applications using HTML, CSS, JavaScript, and Vue.js, collaborating with product <br /> managers and  engineers to deliver scalable, user-friendly tools.</p>
                 </div>
                 <Image src="/anthro.png" alt="" className='w-[70px] h-[70px] mr-3 opacity-50' width={224} height={240}/>
